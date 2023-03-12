e6fd2004ea8921f9e1bde7f779248bb198e5644e
SanjuManna <sanjumanna2201@gmail.com>
2023-03-12 13:51:48 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,54 @@
-import React,{useState,useEffect,useRef} from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../styles/App.css';
 const App = () => {
 
-//code here 
+  const emailRef = useRef();
+  const passRef = useRef();
+
+  const [inputValue, setInputValue] = useState({ email: "", password: "" });
+
+  const [displayMessage, setDisplayMessage] = useState({ emailMsg: "", passwordMeg: "" });
+
+  const handleInputChange = (e) => {
+    e.persist();
+    if (e.target.id === "inputEmail") {
+      setInputValue((state) => {
+        return { ...state, ["email"]: e.target.value };
+      })
+
+    } else {
+      setInputValue((state) => {
+        return { ...state, ["password"]: e.target.value };
+      })
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue.email === "") {
+      emailRef.current.focus();
+      setDisplayMessage({ emailMsg: "", passwordMeg: "" });
+    } else if (inputValue.password === "") {
+      passRef.current.focus();
+      setDisplayMessage({ emailMsg: "", passwordMeg: "" });
+    } else {
+      setDisplayMessage({ emailMsg: inputValue.email, passwordMeg: inputValue.password });
+      setInputValue({ email: "", password: "" });
+    }
+
+  }
+  //code here 
 
   return (
     <div id="main">
       Email
-      <input id="inputEmail" type="text" value={} ref={} onChange={}/><br/>
+      <input id="inputEmail" type="text" value={inputValue.email} ref={emailRef} onChange={handleInputChange} /><br />
       Password
-      <input id="inputPassword" type="text" value={} ref={} onChange={}/><br/>
-      <button id="submitButton" onClick={}>Submit</button><br/>
-      <p id="emailText">Your Email : {}</p>
-      <p id ="passwordText">Your Password : {}</p>
-      
+      <input id="inputPassword" type="text" value={inputValue.password} ref={passRef} onChange={handleInputChange} /><br />
+      <button id="submitButton" onClick={handleSubmit}>Submit</button><br />
+      <p id="emailText">Your Email : {displayMessage.emailMsg}</p>
+      <p id="passwordText">Your Password : {displayMessage.passwordMeg}</p>
+
     </div>
   )
 }
